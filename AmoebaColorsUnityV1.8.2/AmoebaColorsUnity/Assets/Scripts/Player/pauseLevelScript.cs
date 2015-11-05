09a39022b3eb8bbbc04e004889dba4fbae59059b
CAGD Student <cagd495@mailinator.com>
2015-11-05 09:34:14 -0800

    Created Loading Screen & Started Pause Script

---
+++ AmoebaColorsUnityV1.8.2/AmoebaColorsUnity/Assets/Scripts/Player/pauseLevelScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pauseLevelScript : MonoBehaviour {
+
+	public bool gamePause;
+
+	// Use this for initialization
+	void Start () {
+
+		gamePause = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int fingerCount = 0;
+		foreach (Touch touch in Input.touches) {
+			if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled)
+				fingerCount++;
+			
+		}
+		if (fingerCount > 3)
+		{
+			print("User has " + fingerCount + " finger(s) touching the screen");
+			gamePause = true;
+		}
+	}
+}

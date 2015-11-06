4f6786a6a3cbf9ccd8d6506583f3a291b474969d
zgibson2 <zachgibson22@gmail.com>
2015-11-05 18:34:44 -0800

    GitTest - zach

--- AmoebaColorsUnityV1.8.2/AmoebaColorsUnity/Assets/Scripts/Player/playerController.cs
+++ AmoebaColorsUnityV1.8.2/AmoebaColorsUnity/Assets/Scripts/Player/playerController.cs
@@ -3,9 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class playerController : MonoBehaviour {
-
-
-
+	
+	
+	
 	public float baseSpeed = 0.1f; //.1 is normal speed the player can move
 	public float playerSize = 1.0f;	//A player Size 1 is the smallest possible size
 	public bool hasKey = false; //checks if you have a key
@@ -16,9 +16,9 @@ public class playerController : MonoBehaviour {
 	public float respawnTimer = 1.0f;
 	private float myDepth;
 	private Vector3 myTransform;
-
+	
 	private List<GameObject> keyArray;
-
+	
 	//Merged Variables
 	private Vector2 mousePosition2D;
 	private Vector3 mousePosition3D;
@@ -29,101 +29,109 @@ public class playerController : MonoBehaviour {
 	public float magForceMultiplier = 1.0f;
 	private float magneticForce;
 	public float magneticDistance = 4;			// the distance at which the pointer must be to affect an instance of the player
-
+	
 	//Health System
 	private Sprite currentSprite;
 	private bool beenHit = false;
 	private bool playerInvuln;
 	private float knockBackMultiplier = 100.0f;
 	private Vector3 startSize;
-
-
+	
+	
 	private GameObject checkpointCheck;
 	private GameObject currentCheckpoint;
-
+	
 	private Animation playerAnimation;
 	private string displayText;
-
+	
 	private Vector3 touchPosition;
 	private Vector3 reverseTouchPosition;
 	private float playerSpeed;
 	private int touchCount= 0;
 	public bool canLaunch;
-
-
+	
+	private bool touchPlayer;
+	
+	
 	public bool usingTouch = true;		//false if you want to use keyboard
-
+	
 	private string currentPickup = "none";
-
+	
 	public int amoebaColor = 0;		// 0  is neutral (grey), 1 is blue, 2 is yellow
-
-
-
+	
+	
+	
 	public void changeToBlue()
 	{
 		renderer.sprite = cellBlue;
 		amoebaColor = 1;
-
-		//Debug.Log ("Now BLUE Amoeba!");
+		
 	}
 	public void changeToYellow()
 	{
 		renderer.sprite = cellYellow;
 		amoebaColor = 2;
 		
-		//Debug.Log ("Now YELLOW Amoeba!");
 	}
 	public void changeToGreen()
 	{
 		renderer.sprite = cellGreen;
 		amoebaColor = 3;
 		
-		Debug.Log ("Now GREEN Amoeba!");
 	}
-
+	
+	void changeStartColor()
+	{
+		if (amoebaColor==1)
+			changeToBlue();
+		if (amoebaColor==2)
+			changeToYellow();
+		if (amoebaColor==3)
+			changeToGreen();
+		
+	}
+	
 	public void calculateAmeobaSize()			//use to set the scale of the amoeba
 	{
 		float number = playerSize*0.25f;
-
+		
 		transform.localScale =  new Vector3 (number, number, number);
 	}
-
-
-	//IEnumerators Start
-
-
+	
+	
+	
 	public IEnumerator Knockback(float knockDur, float knockbakcPwr, Vector3 knockbackDir, Rigidbody2D knockBody)
 	{
 		float timer = 0;
-
+		
 		while( knockDur > timer)
 		{
 			timer +=Time.deltaTime;
-
+			
 			knockBody.AddForce(new Vector3(knockbackDir.x * knockbakcPwr, knockbackDir.y * knockbakcPwr, transform.position.z));
 		}
-
+		
 		yield return 0;
 	}
-
+	
 	public IEnumerator changePlayerInvuln(float timer)
 	{
 		bool invuln = false;
-
+		
 		
 		yield return new WaitForSeconds(timer);
 		gameObject.GetComponent<playerController>().playerInvuln = invuln;
 		Debug.Log ("changePlayerInvuln = false");
 	}
-
+	
 	public IEnumerator changeBeenHit(float timer)
 	{
 		bool hit = false;
-
+		
 		yield return new WaitForSeconds(timer);
 		gameObject.GetComponent<playerController>().beenHit = hit;
 	}
-
+	
 	public void checkHitStatus()
 	{
 		if(beenHit)
@@ -136,9 +144,9 @@ public class playerController : MonoBehaviour {
 			transform.localScale = startSize;
 		}
 	}
-
-
-
+	
+	
+	
 	private IEnumerator PlayAnimationInterval(int n, float time)
 	{
 		while (n > 0)
@@ -148,9 +156,9 @@ public class playerController : MonoBehaviour {
 			yield return new WaitForSeconds(time);
 		}
 	}
-
+	
 	// IEnumerators End
-
+	
 	void RepelPlayer (bool touchInput)		//if touch == true use touch input
 	{
 		
@@ -182,7 +190,7 @@ public class playerController : MonoBehaviour {
 			}
 		}
 	}
-
+	
 	//Set up all of our sprite vars and update them in the Start function
 	SpriteRenderer renderer;
 	Rigidbody2D myRigidbody;
@@ -190,7 +198,7 @@ public class playerController : MonoBehaviour {
 	Sprite cellBlue;
 	Sprite cellGreen;
 	Sprite cellYellow;
-
+	
 	void Start()
 	{
 		cellGrey = Resources.Load<Sprite>("Art/Sprites/spr_cell_grey");
@@ -202,11 +210,14 @@ public class playerController : MonoBehaviour {
 		startPos = transform.position;
 		playerAnimation = GetComponent<Animation>();
 		startSize = transform.localScale;
-
+		
+		changeStartColor();
+		
+		touchPlayer = false;
 	}
-
+	
 	void FixedUpdate(){
-
+		
 		myTransform = transform.position;
 		myTransform.z = 0;
 		transform.position = myTransform;
@@ -227,7 +238,7 @@ public class playerController : MonoBehaviour {
 			
 			if (Input.GetKey ("p"))
 				Debug.Log(amoebaColor);
-
+			
 			if (Input.GetMouseButton(0))
 			{
 				mousePosition3D = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));  
@@ -236,12 +247,8 @@ public class playerController : MonoBehaviour {
 				{
 					transform.position = Vector3.Lerp(transform.position, mousePosition3D, (Time.deltaTime*baseSpeed*5)/playerSize);
 				}
-				else
-				{
-					RepelPlayer(usingTouch);
-				}
 			}
-
+			
 		} 
 		else
 		{
@@ -249,50 +256,30 @@ public class playerController : MonoBehaviour {
 			if (Input.touchCount > 0)
 			{
 				// The screen has been touched so store the touch
-				Touch touch1 = Input.GetTouch (0);
-
-				if (amoebaColor == 2)
-				{
-					RaycastHit hit;
-					Ray ray = Camera.main.ScreenPointToRay(touch1.deltaPosition);
-					if (Physics.Raycast(ray, out hit))
-						if (hit.collider != null)
-							if (hit.collider.gameObject.tag == "Player")
-							{
-								Debug.Log ("Yayyy");
-							}
-					}
-
-
-				if (touch1.phase == TouchPhase.Stationary || touch1.phase == TouchPhase.Moved)
+				Touch touch = Input.GetTouch (0);
+				
+				if  (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)
 				{
 					// If the finger is on the screen, move the object smoothly to the touch position
-					touchPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch1.position.x, touch1.position.y, 10));  
+					touchPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 10));  
 					//reverseTouchPosition = new Vector3 (-touchPosition.x, -touchPosition.y, 10);
-						Vector3 direction = (touchPosition - transform.position).normalized * baseSpeed * Time.smoothDeltaTime * 2200;
-						myRigidbody.velocity = direction;		
-
-				}
-				else
-				{
-					RepelPlayer(usingTouch);
+					Vector3 direction = (touchPosition - transform.position).normalized * baseSpeed * Time.smoothDeltaTime * 2200;
+					myRigidbody.velocity = direction;		
+					
 				}
 			}
 			else
 			{
 				myRigidbody.velocity = Vector3.zero;
 			}
-			if (Input.touchCount > 3)
-			{
-				Application.LoadLevel(0);
-			}
 		}
 	}
-
+	
 	void Update () {
-
+		
 		touchCount = 0;
-
+		
+		/*
 		foreach (Touch touch in Input.touches) //Tracks how many fingers touching
 		{
 			if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled)
@@ -311,13 +298,13 @@ public class playerController : MonoBehaviour {
 		{
 			//changeToBlue();
 			canLaunch = true;
-		}
+		}*/
 		//------end CONTROLS----------
-
-
+		
+		
 		//Debug.Log (playerSpeed);
 		calculateAmeobaSize();
-
+		
 		if (startRespawnTimer = true)
 		{
 			respawnTimer -= Time.deltaTime;
@@ -326,35 +313,35 @@ public class playerController : MonoBehaviour {
 				RespawnTimerEnd();
 			}
 		}
-
+		
 		//Checking if hit to change size
 		checkHitStatus();
-
-
+		
+		
 	}	//end update function
-
+	
 	void RespawnTimerEnd()
 	{
 		dropObjects = false;
 		startRespawnTimer = false;
 		respawnTimer = 1.0f;
 	}
-
+	
 	//---------------PICK UPS & COLLISIONS-------------------------
-
-
+	
+	
 	void OnCollisionEnter2D (Collision2D other)
 	{
 		if (other.gameObject.CompareTag ("Power Up"))
 		{
 			powerUpTypeScript powerUpRef = other.gameObject.GetComponent<powerUpTypeScript>();
-
+			
 			powerUpRef.playParticle();
-
+			
 			if (powerUpRef.powerUpType == 1)
 			{
 				changeToBlue();
-			
+				
 				other.gameObject.SetActiveRecursively (false);
 			}
 			else
@@ -374,48 +361,48 @@ public class playerController : MonoBehaviour {
 				
 				other.gameObject.SetActiveRecursively (false);
 			}
-
+			
 		}
-
+		
 		if (other.gameObject.CompareTag ("Pick Up"))
 		{			
 			if (amoebaColor == 1)	//we can only pick stuff up if we are blue(1)
 			{
 				currentPickup = other.gameObject.GetComponent<pickupHolderScript>().pickupName;
 				//currentPickup = pickupNameRef.pickupName;							//store the pickups name in a string
-
+				
 				//keyArray.Add(other.gameObject);
-
+				
 				if(other.gameObject.GetComponent<pickupFollowMovement>().canAdd == true)
-					{
-						numberOfKeys ++;
-						Debug.Log (numberOfKeys);
-						other.gameObject.GetComponent<pickupFollowMovement>().canAdd = false;
-
-					}
-					if(other.gameObject.GetComponent<pickupFollowMovement>().isFollowing == true)
-					{						
-						hasKey = true;
-					}
+				{
+					numberOfKeys ++;
+					Debug.Log (numberOfKeys);
+					other.gameObject.GetComponent<pickupFollowMovement>().canAdd = false;
+					
+				}
+				if(other.gameObject.GetComponent<pickupFollowMovement>().isFollowing == true)
+				{						
+					hasKey = true;
+				}
 				Debug.Log ("Now holding: " + currentPickup);
 			}
 		}
-
+		
 		if (other.gameObject.CompareTag ("Door"))
 		{
 			doorLockedScript doorRef = other.gameObject.GetComponent<doorLockedScript>();
-
-
+			
+			
 			//int tempKey = keyArray.Count;
 			Debug.Log (numberOfKeys);
 			//keyArray.RemoveAt(tempKey);
-			 
-
+			
+			
 			if (numberOfKeys > 0)
 			{
 				other.gameObject.SetActive (false);
-
-
+				
+				
 			}
 		}
 		if (other.gameObject.CompareTag ("Destructable"))
@@ -427,14 +414,14 @@ public class playerController : MonoBehaviour {
 		}
 		if (other.gameObject.CompareTag ("Dangerous"))
 		{
-
+			
 			if (!playerInvuln)
 			{
 				if (!beenHit) 
 				{
 					Vector3 dir = other.gameObject.transform.position - transform.position;
 					dir = dir.normalized;
-
+					
 					StartCoroutine(Knockback(0.02f, knockBackMultiplier, transform.position, myRigidbody));
 					beenHit = true;
 					playerInvuln = true;
@@ -445,7 +432,7 @@ public class playerController : MonoBehaviour {
 					numberOfKeys = 0;
 					dropObjects = true;
 					beenHit = false;
-
+					
 					if (currentCheckpoint == null)
 					{
 						startRespawnTimer = true;
@@ -468,6 +455,5 @@ public class playerController : MonoBehaviour {
 				currentCheckpoint.GetComponent<checkPointActive>().currentPoint = true;
 			}
 		}
-		
 	}
 }
